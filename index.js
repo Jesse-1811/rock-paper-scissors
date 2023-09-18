2a4993bb1538ae8c3d25dd02a9fc8f1815434102
Jesse_1811 <101838696+Jesse-1811@users.noreply.github.com>
2023-09-18 14:48:32 +0100

    Add player variable and game round logic

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+
+
+console.log(computerChoice);
+let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+
+function gameRound(playerSelection,  getComputerChoice) {
+    if (getComputerChoice === "rock" && playerSelection === "paper"){
+        console.log("You win!, Paper beats rock!");
+    } else if (getComputerChoice === "rock" && playerSelection === "scissors"){
+        console.log("You lose!, Rock beats scissors!");
+    } else if (getComputerChoice === "paper" && playerSelection === "rock"){
+        console.log("You lose!, paper beats rock!")
+    } else if (getComputerChoice === "paper" && playerSelection === "scissors"){
+        console.log("You win!, scissors beats paper!")
+    } else if (getComputerChoice === "scissors" && playerSelection === "rock"){
+        console.log("You win!, rock beats scissors!")
+    } else if (getComputerChoice === "scissors" && playerSelection === "paper"){
+        console.log("You lose!, scissors beats paper!")
+    } else if (getComputerChoice === playerSelection){
+        console.log("Its a draw!");
+    } else {
+        console.log("Please select a valid option")
+    }
+}
