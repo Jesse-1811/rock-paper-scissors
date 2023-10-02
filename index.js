12cc3a7aede514ec79fee55d32acd25a06b694ca
Jesse_1811 <101838696+Jesse-1811@users.noreply.github.com>
2023-10-02 18:34:58 +0100

    Print the result of the round

--- index.js
+++ index.js
@@ -10,17 +10,12 @@ function getComputerChoice() {
     }
 }
 
-
-// let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-// let computerSelection = getComputerChoice();
-
 let userScore = 0;
 let compScore = 0;
 
 function game() { 
   let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
   let computerSelection = getComputerChoice();
-    // function gameRound(playerSelection, computerSelection) {
         if (computerSelection === "rock" && playerSelection === "paper"){
             userScore++;
             console.log("You win! Paper beats rock!");
@@ -45,8 +40,6 @@ function game() {
             console.log("Please select a valid option");
         }
     
-
-
 }
 
 function gameRounds() {
@@ -54,37 +47,18 @@ function gameRounds() {
         game();
     }
 }
+
+function winner(userScore, compScore) {
+    if (userScore > compScore) {
+        return "You won this round!"
+    } else if (userScore < compScore) {
+        return "You lost this round!"
+    } else {
+        return "This round is a draw!"
+    }
+}
+
 gameRounds()
 console.log("Your score: " + userScore);
 console.log("Computers score: " + compScore);
-
-
-// 5 round game
-// Tracks wins and losses for each round
-// Prints the winner at the end of the game
-
-// function gameRound(playerSelection, computerSelection) {
-    // if (computerSelection === "rock" && playerSelection === "paper"){
-        // count++;
-        // return "You win!, Paper beats rock!";
-    // } else if (computerSelection === "rock" && playerSelection === "scissors"){
-    //     count--;
-    //     return "You lose!, Rock beats scissors!";
-    // } else if (computerSelection === "paper" && playerSelection === "rock"){
-    //     count--;
-    //     return "You lose!, paper beats rock!";
-    // } else if (computerSelection === "paper" && playerSelection === "scissors"){
-    //     count++;
-    //     return "You win!, scissors beats paper!";
-    // } else if (computerSelection === "scissors" && playerSelection === "rock"){
-    //     count++;
-    //     return "You win!, rock beats scissors!";
-    // } else if (computerSelection === "scissors" && playerSelection === "paper"){
-    //     count--;
-    //     return "You lose!, scissors beats paper!";
-    // } else if (computerSelection === playerSelection){
-    //     return "Its a draw!";
-    // } else {
-    //     return "Please select a valid option";
-    // }
-// }
+console.log(winner(userScore, compScore));
